5dc71ae9554fc5ab901e3d81d6c5cd1bc8d2219b
zhangge1998 <1263061442@qq.com>
2017-07-21 11:08:38 +0800

    test function printReceiptTxt

--- pos_v1/test/main_test1.js
+++ pos_v1/test/main_test1.js
@@ -212,3 +212,63 @@ describe('buildTotalStatus', ()=> {
     expect(buildTotalStatus(itemTotal)).toEqual(total);
   });
 })
+describe("printReceiptTxt",()=>{
+  let total = {
+    list: [
+      {
+        cartItem: {
+          item: {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+          },
+          count: 5
+        },
+        subtotal: 12,
+        itemDiscount: 3
+      },
+      {
+        cartItem: {
+          item: {
+            barcode: 'ITEM000003',
+            name: '荔枝',
+            unit: '斤',
+            price: 15.00
+          },
+          count: 2
+        },
+        subtotal: 30,
+        itemDiscount: 0
+      },
+      {
+        cartItem: {
+          item: {
+            barcode: 'ITEM000005',
+            name: '方便面',
+            unit: '袋',
+            price: 4.5
+          },
+          count: 3
+        },
+        subtotal: 9,
+        itemDiscount: 4.5,
+      },
+    ],
+    total: 51,
+    discount: 7.5
+  };
+  it('should return right txt',()=>{
+
+    const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：51.00(元)
+节省：7.50(元)
+**********************`;
+
+    expect(printReceiptTxt(total)).toEqual(expectText);
+  })
+})
