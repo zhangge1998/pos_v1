fd9a2310121b3a1894eea61014c086ff8d24e162
zhangge1998 <1263061442@qq.com>
2017-07-21 11:05:25 +0800

    test function buildTotalStatus

--- pos_v1/test/main_test1.js
+++ pos_v1/test/main_test1.js
@@ -121,4 +121,94 @@ describe('buildItemStatus', ()=> {
     expect(buildItemTotalStatus(itemCount)).toEqual(itemTotal);
   })
 });
-
+describe('buildTotalStatus', ()=> {
+  let itemTotal = [
+    {
+      cartItem: {
+        item: {
+          barcode: 'ITEM000001',
+          name: '雪碧',
+          unit: '瓶',
+          price: 3.00
+        },
+        count: 5
+      },
+      subtotal: 12,
+      itemDiscount: 3
+    },
+    {
+      cartItem: {
+        item: {
+          barcode: 'ITEM000003',
+          name: '荔枝',
+          unit: '斤',
+          price: 15.00
+        },
+        count: 2
+      },
+      subtotal: 30,
+      itemDiscount: 0
+    },
+    {
+      cartItem: {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.5
+        },
+        count: 3
+      },
+      subtotal: 9,
+      itemDiscount: 4.5,
+    },
+  ];
+  it('should return total', ()=> {
+    const total = {
+      list: [
+        {
+          cartItem: {
+            item: {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00
+            },
+            count: 5
+          },
+          subtotal: 12,
+          itemDiscount: 3
+        },
+        {
+          cartItem: {
+            item: {
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00
+            },
+            count: 2
+          },
+          subtotal: 30,
+          itemDiscount: 0
+        },
+        {
+          cartItem: {
+            item: {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.5
+            },
+            count: 3
+          },
+          subtotal: 9,
+          itemDiscount: 4.5,
+        },
+      ],
+      total: 51,
+      discount: 7.5
+    };
+    expect(buildTotalStatus(itemTotal)).toEqual(total);
+  });
+})
